6afd1e1ee7f0b2b2daffa20020cf7981f2be1656
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-11-03 22:15:32 -0700

    added login verification text added global enter button submission

--- public/login.js
+++ public/login.js
@@ -1,7 +1,33 @@
-let login = function(username, password) {
+let login = async function(username, password) {
     sessionStorage.setItem("username", username);
     sessionStorage.setItem("password", password);
     window.location.href = "/view";
+}
+
+let verify_user = function(username, password) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/user");
+
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onload = function() {
+        if (xhr.status == 202) {
+            login(username, password)
+        }
+        display_error(xhr.responseText);
+    };
+
+    let data = {
+        "username": username,
+        "password": password 
+    };
+
+    xhr.send(JSON.stringify(data));
+}
+
+let display_error = function(error) {
+    document.getElementById("error").innerText = error;
 }
 
 window.onload = function() {
@@ -15,3 +41,13 @@ window.onload = function() {
         window.location.href = "/view";
     }
 }
+
+document.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("loginbtn").click();
+    }
+  }); 
